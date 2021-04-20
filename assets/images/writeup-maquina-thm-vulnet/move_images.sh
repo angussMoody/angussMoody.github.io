b66f2a80d2faca2cbb3b1c35d92ebf42bcd45ec7
abeljm <abeljm2017@gmail.com>
2021-04-19 22:36:25 -0500

    upload images writeup VulnNet

---
+++ assets/images/writeup-maquina-thm-vulnet/move_images.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+while true
+do
+  mv *.* /home/pentester/Documents/github/abeljm.github.io/assets/images/writeup-maquina-thm-vulnet
+  echo "mv *.* /home/pentester/Documents/github/abeljm.github.io/assets/images/writeup-maquina-thm-vulnet"
+  sleep 5
+done
